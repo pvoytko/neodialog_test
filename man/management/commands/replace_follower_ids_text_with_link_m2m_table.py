589a2d2e10211f2a0033b06c41181117e8d6160d
pvoytko <pvoytko@gmail.com>
2013-07-08 16:18:55 +0400

    Z-1 Добавлены файлы с настройками для хостинга Джино

--- man/management/commands/replace_follower_ids_text_with_link_m2m_table.py
+++ man/management/commands/replace_follower_ids_text_with_link_m2m_table.py
@@ -27,9 +27,16 @@ class Command(BaseCommand):
         # Первый проход - создаем объекты
         for m in Man.objects.all():
             print m.id, m.name
-            #newM = man.models.Man2(id=m.id, name=m.name)
-            #newM.save()
+            newM = man.models.Man2(id=m.id, name=m.name)
+            newM.save()
 
         # Второй проход - создаем связи
         for m in Man.objects.all():
-            print m.follow_ids
+            print m.id, m.name, "{0}".format(m.follow_ids),
+            if len(m.follow_ids) > 0:
+                ids = m.follow_ids.split(' ')
+                for id in ids:
+                    man.models.Man2.objects.get(id=m.id).forward_mans.add(man.models.Man2.objects.get(id=id))
+                print len(ids)
+            else:
+                print 'skip'
